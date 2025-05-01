21aa9583fd8fd83ca8fbbe6f238a8bbc30e13d5c
EgorSad06 <143409348+EgorSad06@users.noreply.github.com>
2025-05-01 22:36:19 +0300

    меню и настройки
    Меню визуально полностью доделано. Добавлено меню настроек.
    
    Из проблем, которые надо поправить:  кнопка назад не работает и яркость в настройках не работает, хотя ошибки мб дело в настройках компа или типо того

--- FD_FE/FD_MainWindow/MainMenu.xaml.cs
+++ FD_FE/FD_MainWindow/MainMenu.xaml.cs
@@ -17,6 +17,7 @@ namespace FD_MainWindow
 {
     /// <summary>
     /// Логика взаимодействия для MainMenu.xaml
+    /// файл настройки кнопок и функций Меню.
     /// </summary>
     public partial class MainMenu : Page
     {
@@ -24,10 +25,48 @@ namespace FD_MainWindow
         {
             InitializeComponent();
         }
-
-        private void CardSelection_Click(object sender, RoutedEventArgs e)
+        // кнопка играть, переход к созданию комнаты
+        private void PlayButtonSelection_Click(object sender, RoutedEventArgs e)
         {
 
         }
+        //кнопка обучение, переход к обучению
+        private void EducationButtonSelection_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // Кнопка статистики, переход к просмотру статистики 
+        private void StatisticsButtonSelection_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //Кнопка Настройки, переход к настройкам игры
+        private void SettingsButtonSelection_Click(object sender, RoutedEventArgs e)
+        {
+            // Переход на страницу настроек
+            NavigationService.Navigate(new Uri("Setting.xaml", UriKind.Relative));
+
+            // Если нужно закрыть текущую страницу:
+            NavigationService.RemoveBackEntry();
+        }
+        //выход из игры
+        private void ExitButtonSelection_Click(object sender, RoutedEventArgs e)
+        {
+            // предупреждение о выходе
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите выйти?", "Подтверждение",
+                                           MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            // проверка ответа пользователя
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+
+        }
+
+
+
+
+
     }
 }
---
+++ FD_FE/FD_MainWindow/Setting.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FD_MainWindow
+{
+    public partial class Setting : Page
+    {
+        // Коллекция доступных разрешений
+        public ObservableCollection<Resolution> Resolutions { get; } = new ObservableCollection<Resolution>()
+        {
+            new Resolution(1920, 1080),
+            new Resolution(1280, 720),
+            new Resolution(1600, 900)
+        };
+
+        // Текущие настройки
+        private int _brightness = 80;
+        private int _masterVolume = 100;
+        private int _effectsVolume = 100;
+
+        public Setting()
+        {
+            InitializeComponent();
+
+            // Инициализация элементов управления
+            ResolutionComboBox.ItemsSource = Resolutions;
+            ResolutionComboBox.SelectedIndex = 0;
+
+            //В будующем можно добавить звуки эффектов, музыку 
+
+            WindowedRadioButton.IsChecked = false;
+            //BrightnessSlider.Value = _brightness;
+            //MasterVolumeSlider.Value = _masterVolume;
+            //EffectsVolumeSlider.Value = _effectsVolume;
+        }
+
+        // Обработчики событий
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow;
+
+            // Режим окна
+            if (WindowedRadioButton.IsChecked == true)
+            {
+                mainWindow.WindowState = WindowState.Normal;
+                mainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+                mainWindow.ResizeMode = ResizeMode.CanResize;
+            }
+            else
+            {
+                mainWindow.WindowState = WindowState.Maximized;
+                mainWindow.WindowStyle = WindowStyle.None;
+                mainWindow.ResizeMode = ResizeMode.NoResize;
+            }
+
+            MessageBox.Show("Настройки применены!", "Успех",
+               MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Сброс к значениям по умолчанию
+            ResolutionComboBox.SelectedIndex = 0;
+            WindowedRadioButton.IsChecked = true;
+            //BrightnessSlider.Value = 80;
+            //MasterVolumeSlider.Value = 100;
+            //EffectsVolumeSlider.Value = 100;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Возврат на предыдущую страницу
+            NavigationService.GoBack();
+        }
+
+        // Класс для хранения разрешений
+        public class Resolution
+        {
+            public int Width { get; }
+            public int Height { get; }
+            public string DisplayName => $"{Width}x{Height}";
+
+            public Resolution(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+    }
+}
