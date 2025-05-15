99144f25835d21712093a40c5e01f9672e87f02d
barsik-47 <alekzzandr07@gmail.com>
2025-05-15 17:11:33 +0300

    Work

--- FD_FE/FD_MainWindow/GameplayResources/UCCard.xaml.cs
+++ FD_FE/FD_MainWindow/GameplayResources/UCCard.xaml.cs
@@ -23,14 +23,14 @@ namespace FD_MainWindow
     {
         public UCCard(BoardCard card, double x, double y)
         {
-            this.BoardCard = card;
+            BoardCard = card;
             InitializeComponent();
-            string path = $"{GameplayData.sprites_path}CardTemplates/{this.BoardCard.fraction}_template.png";
+            string path = $"{GameplayData.sprites_path}CardTemplates/{BoardCard.fraction}_template.png";
             CardBackgound.ImageSource = (ImageSource)MainWindow.converter.ConvertFromString(path);
-            path = $"{GameplayData.sprites_path}CardTemplates/{this.BoardCard.card_class}_frame.png";
+            path = $"{GameplayData.sprites_path}CardTemplates/{BoardCard.card_class}_frame.png";
             CardClassFrame.ImageSource = (ImageSource)MainWindow.converter.ConvertFromString(path);
-            CardImage.Source = (ImageSource)MainWindow.converter.ConvertFromString($"{GameplayData.sprites_path}Cards/{this.BoardCard.image}");
-            this.Margin = new Thickness(x, y, 0,0);
+            CardImage.Source = (ImageSource)MainWindow.converter.ConvertFromString($"{GameplayData.sprites_path}Cards/{BoardCard.image}");
+            Margin = new Thickness(x, y, 0,0);
         }
         
         public static DependencyProperty BoardCardProperty;
@@ -43,7 +43,7 @@ namespace FD_MainWindow
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             BoardAct.selected_card = BoardCard;
-            BoardCard.AV += 1;
+            BoardCard.AV++;
         }
     }
 }
